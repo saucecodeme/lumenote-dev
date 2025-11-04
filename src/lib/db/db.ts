574e5916f7e4c1c1d3508872a0c3214a102af757
godccino <supitchaklanpradit@outlook.com>
2025-11-05 00:44:40 +0700

    init lumenote

---
+++ src/lib/db/db.ts
@@ -0,0 +1,39 @@
+import Dexie, { type Table} from 'dexie';
+
+export type BlockType = 'text' | 'heading' | 'todo' | 'code'
+export type Block = {
+  id: string, // ulid
+  docId: string,
+  type: BlockType,
+  content: string,
+  meta?: {
+    level?: 1 | 2 | 3; // for headings
+    checked?: boolean; // for todos
+    language?: string; // for code
+  };
+  order: number, // sortable index
+  createdAt: number,
+  updatedAt: number,
+}
+
+export type Document = {
+  id: string, // ulid
+  title: string,
+  createdAt: number,
+  updatedAt: number,
+}
+
+class DexieDB extends Dexie {
+  documents!: Table<Document, string>;
+  blocks!: Table<Block, string>;
+
+  constructor() {
+    super('lumenote');
+    this.version(1).stores({
+      documents: 'id, updatedAt',
+      blocks: 'id, docId, order, updatedAt',
+    })
+  }
+}
+
+export const db = new DexieDB();
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({ component: App })
+
+function App() {
+
+  return (
+    <div className="min-h-screen p-10">
+
+    </div>
+  )
+}
